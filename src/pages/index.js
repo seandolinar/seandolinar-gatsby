f86ef4eba5f58a07100f55fd38d129e479ba601f
seandolinar <sjjd199@gmail.com>
2018-11-25 15:00:51 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <>seandolinar.com<br/><a href="//teststatic.seandolinar.com">stats.seandolinar.com</a></>
+)
+
+export default IndexPage
