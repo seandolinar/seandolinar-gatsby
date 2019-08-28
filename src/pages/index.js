a63f53ae054d01378ebf13e19e79050c74755526
seandolinar <sjjd199@gmail.com>
2019-08-27 21:27:06 -0400

    added job descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import '../styles/index.scss';
 const IndexPage = () => (
   <div className="wrapper-index">
     <h1 className="site-title">Sean Dolinar</h1>
+    <h3>Front End | Data Visualization | Mobile </h3>
     <div></div>
     <div className="site-blog"><span>blog: </span><a href="//stats.seandolinar.com">stats.seandolinar.com</a></div>
   </div>
